ea9179649e609bc918946b61b35f6b56b2cc06f9
John Beech <john.beech@bbc.co.uk>
2016-10-29 17:50:34 +0100

    Side goal: Auto decode encoded images after generation

--- start.js
+++ start.js
@@ -6,15 +6,6 @@ var fs = require('fs');
 var encodeImage = require('./lib/encodeImage');
 var decodeImage = require('./lib/decodeImage');
 
-var sourceImagePath = __dirname + '/images/source/workstation.jpg';
-var outputPath = __dirname + '/images/output';
-var outputName = 'save';
-
-var messageToEncode = process.argv[2] || 'Team Fate is Awesome! Hack Manchester 2016';
-var dataToEncode = createNumberArrayFromString(messageToEncode);
-var decodedData = dataToEncode; // TODO: Read from source image
-var decodedMessage = createStringFromNumberArray(decodedData);
-
 function createNumberArrayFromString(inputString) {
   var numberArray = [];
   for (var i = 0; i < inputString.length; i++) {
@@ -31,38 +22,68 @@ function createStringFromNumberArray(numberArray) {
   return stringArray.join('');
 }
 
+var sourceImagePath = __dirname + '/images/source/workstation.jpg';
+var outputPath = __dirname + '/images/output';
+var outputName = 'save';
+
+var messageToEncode = process.argv[2] || 'Team Fate is Awesome! Hack Manchester 2016';
+var dataToEncode = createNumberArrayFromString(messageToEncode);
+var decodedData = dataToEncode; // TODO: Read from source image
+var decodedMessage = createStringFromNumberArray(decodedData);
+
 console.log('Message to Encode:', messageToEncode);
 console.log('Data to Encode:');
 console.log(dataToEncode.join(' '));
-console.log('Decoded Data:');
+console.log('Sample Decoded Data:');
 console.log(decodedData.join(' '));
-console.log('Decoded Message:');
+console.log('Sample Decoded Message:');
 console.log(decodedMessage.split('').join(' '));
 
-fs.readFile(sourceImagePath, function (err, sourceImage) {
-  if (err) throw err;
-
+function encodeImageFile(imagePath) {
   console.log('');
+  return new Promise((accept, reject) => {
+    fs.readFile(imagePath, function (err, sourceImage) {
+      if (err) reject(err);
 
-  encodeImage(sourceImage, dataToEncode, outputPath, outputName)
-    .then((files) => {
-      console.log('');
-      console.log(`Saved output as ${files.length} files:`, NL, files.join(NL));
-      return files;
-    })
-    .then((files) => {
-      console.log('');
-      console.log('Decoding data', files[0]);
-      fs.readFile(files[0], function (err, sourceImage) {
-        return decodeImage(sourceImage).then((decodedData) => {
-          // console.log('Decoded Data:', decodedData.join(' '));
+      encodeImage(sourceImage, dataToEncode, outputPath, outputName)
+        .then((files) => {
+          console.log('');
+          console.log(`Saved output as ${files.length} files:`, NL, files.join(NL));
+          return files;
+        })
+        .then(accept)
+        .catch(reject);
+    });
+  });
+}
+
+function decodeImageFile(imagePath) {
+  console.log('');
+  console.log('Decoding data from:', imagePath);
+
+  return new Promise((accept, reject) => {
+    fs.readFile(imagePath, function (err, sourceImage) {
+      if (err) reject(err);
+
+      decodeImage(sourceImage).then((decodedData) => {
           console.log('Decoded data', decodedData.length, 'values');
           var decodedMessage = createStringFromNumberArray(decodedData);
           console.log('Decoded Message:', decodedMessage);
-        });
-      });
-    })
-    .catch((ex) => {
-      console.error('Explosions', ex, ex.stack);
+          console.log('');
+        })
+        .then(accept)
+        .catch(reject);
     });
-});
+  });
+}
+
+encodeImageFile(sourceImagePath)
+  .then((files) => {
+    console.log('Starting to decode', files.length, 'files...');
+    return Promise.all(files.map((file) => {
+        return decodeImageFile(file);
+      }))
+      .catch((ex) => {
+        console.error('Explosions', ex, ex.stack);
+      })
+  });
