4cf723bf86df9381f6cb2a0fb5e351c9baf49bd4
John Beech <john.beech@bbc.co.uk>
2016-10-29 14:43:27 +0100

    Load image, crop, add white text, save to HTML

--- start.js
+++ start.js
@@ -1,19 +1,36 @@
 console.log('node canvas test:');
 
+var fs = require('fs');
 var Canvas = require('canvas')
   , Image = Canvas.Image
   , canvas = new Canvas(200, 200)
   , ctx = canvas.getContext('2d');
 
-ctx.font = '30px Impact';
-ctx.rotate(.1);
-ctx.fillText("Awesome!", 50, 100);
 
-var te = ctx.measureText('Awesome!');
-ctx.strokeStyle = 'rgba(0,0,0,0.5)';
-ctx.beginPath();
-ctx.lineTo(50, 102);
-ctx.lineTo(50 + te.width, 102);
-ctx.stroke();
+var testTemplate = fs.readFileSync(__dirname + '/test.template.html', 'utf8');
 
-console.log('<img src="' + canvas.toDataURL() + '" />');
+var sourceImagePath = __dirname + '/images/source/workstation.jpg';
+var outputTestPath = __dirname + '/test.html'
+fs.readFile(sourceImagePath, function(err, sourceImage) {
+  if (err) throw err;
+  img = new Image;
+  img.src = sourceImage;
+  ctx.drawImage(img, 0, 0, img.width / 4, img.height / 4);
+
+  ctx.font = '30px Impact';
+  ctx.fillStyle = 'rgba(255,255,255,0.8)';
+  ctx.rotate(.1);
+  ctx.fillText("Awesome!", 50, 100);
+
+  var te = ctx.measureText('Awesome!');
+  ctx.strokeStyle = 'rgba(255,255,255,0.8)';
+  ctx.beginPath();
+  ctx.lineTo(50, 106);
+  ctx.lineTo(50 + te.width, 106);
+  ctx.stroke();
+
+  var imgData = canvas.toDataURL();
+  var imgTag = '<img src="' + imgData + '" />';
+  fs.writeFile(outputTestPath, testTemplate.replace('{{body}}', imgTag), 'utf8');
+  console.log('Wrote test image out to', outputTestPath, 'containing', imgData.length, 'bytes');
+});
