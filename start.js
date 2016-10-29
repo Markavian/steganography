3d3fffffcf6f408ac89f9cd784babe52f2f162c8
John Beech <john.beech@bbc.co.uk>
2016-10-29 16:28:36 +0100

    Refactor save methods to their own files

--- start.js
+++ start.js
@@ -4,6 +4,8 @@ var fs = require('fs');
 var Canvas = require('canvas'),
   Image = Canvas.Image;
 
+var savePNG = require('./lib/savePng');
+var saveJPEG = require('./lib/saveJpeg');
 
 var testTemplate = fs.readFileSync(__dirname + '/test.template.html', 'utf8');
 
@@ -97,51 +99,3 @@ fs.readFile(sourceImagePath, function (err, sourceImage) {
     console.log('Wrote files', NL, files.join(NL));
   });
 });
-
-function savePNG(path, canvas) {
-  return new Promise((accept, reject) => {
-    var fs = require('fs'),
-      out = fs.createWriteStream(path),
-      stream = canvas.pngStream();
-
-    stream.on('data', function (chunk) {
-      out.write(chunk);
-    });
-
-    stream.on('end', function () {
-      console.log('Saved png', path);
-      accept(path);
-    });
-
-    setTimeout(() => {
-      reject(`Timed out trying to Save png: ${path}`);
-    }, 5000);
-  });
-}
-
-function saveJPEG(path, canvas, quality) {
-  return new Promise((accept, reject) => {
-    quality = quality || 75;
-    path = path.replace('{{quality}}', quality);
-    var fs = require('fs'),
-      out = fs.createWriteStream(path),
-      stream = canvas.jpegStream({
-        bufsize: 4096, // output buffer size in bytes, default: 4096
-        quality: quality, // JPEG quality (0-100) default: 75
-        progressive: false // true for progressive compression, default: false
-      });
-
-    stream.on('data', function (chunk) {
-      out.write(chunk);
-    });
-
-    stream.on('end', function () {
-      console.log('Saved jpg', path);
-      accept(path);
-    });
-
-    setTimeout(() => {
-      reject(`Timed out trying to Save jpg: ${path}, Quality: ${quality}`);
-    }, 5000);
-  });
-}
